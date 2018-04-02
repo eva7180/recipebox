c136057c7b779d8130e5522f9d69fb3e3e68eb24
Eva Stolz <evasmail@gmail.com>
2018-04-02 21:06:03 +0200

    Add users and finish styles

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,6 @@
 class Recipe < ApplicationRecord
+	belongs_to :user
+
 	has_many :ingredients, inverse_of: :recipe
 	has_many :directions, inverse_of: :recipe
 	accepts_nested_attributes_for :ingredients, reject_if: :all_blank, allow_destroy: true
