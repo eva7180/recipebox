236004d5efe2552dc4459e288f67d3cc2e9da897
Eva Stolz <evasmail@gmail.com>
2018-08-28 12:49:21 +0200

    small change to recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -6,7 +6,7 @@ class Recipe < ApplicationRecord
 	accepts_nested_attributes_for :ingredients, reject_if: :all_blank, allow_destroy: true
 	accepts_nested_attributes_for :directions, reject_if: proc { |attributes| attributes['step'].blank? }, allow_destroy: true
 	
-	validates :title, :description, :image, presence: :true
+	validates :title, :description, presence: :true
 
 	has_attached_file :image, styles: { medium: "400x400#" }
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
