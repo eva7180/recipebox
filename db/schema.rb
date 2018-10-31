1a44d889759ac5dc7cfbc45c352ca267ed934753
Eva Stolz <evasmail@gmail.com>
2018-10-31 21:54:44 +0100

    db setup

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180402175956) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "directions", force: :cascade do |t|
     t.text "step"
     t.integer "recipe_id"
